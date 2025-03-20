2aa1100317038665bd8166eed72b08371f2661dd
rmmuana <romnickmuana@gmail.com>
2025-03-21 02:32:51 +0800

    Modify icon for web and its title

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Payroll System',
       debugShowCheckedModeBanner: false,
       home: const LoginScreen()
     );
